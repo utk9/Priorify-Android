bd66b4a15884d762440e5e90659c2048be9cea33
utk9 <utkarshlamba.2@gmail.com>
2015-11-14 17:58:32 -0500

    added fragment for the rest of the details

--- app/src/main/java/utk/com/priorify/ItemDetailsActivity.java
+++ app/src/main/java/utk/com/priorify/ItemDetailsActivity.java
@@ -1,7 +1,12 @@
 package utk.com.priorify;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by utk on 15-11-14.
@@ -11,5 +16,19 @@ public class ItemDetailsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.itemdetailsactivity_layout);
+        final Spinner spinner = (Spinner) findViewById(R.id.categorySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.parent, new DependentDetailsFragment())
+                    .commit();
+        }
     }
 }
