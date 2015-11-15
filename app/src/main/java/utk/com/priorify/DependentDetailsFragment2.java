b61beb7892a179bea95f15b4d1621bbe5edca739
melissali343 <melissa.li343@outlook.com>
2015-11-14 22:44:22 -0500

    added spinner and calendar to add task screen layout

--- app/src/main/java/utk/com/priorify/DependentDetailsFragment2.java
+++ app/src/main/java/utk/com/priorify/DependentDetailsFragment2.java
@@ -1,19 +1,39 @@
 package utk.com.priorify;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by utk on 15-11-14.
  */
 public class DependentDetailsFragment2 extends Fragment {
     @Nullable
+
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dependentfragment2_layout, container, false);
+
+        View v= inflater.inflate(R.layout.dependentfragment2_layout, container, false);
+
+        Spinner spinner = (Spinner) v.findViewById(R.id.timeSpinner2);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.estimtime_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        return v;
     }
 }
